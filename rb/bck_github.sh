662ba38af001ec2d06230f75ca84f72754589b57
Romain Boman <romain.boman@gmail.com>
2016-07-26 17:25:28 +0200

    backup repositories

--- rb/bck_github.sh
+++ rb/bck_github.sh
@@ -14,19 +14,21 @@ cd gits-$DATE
 
 function f_github()
 {
-    if [ "x$3" != "x" ]
-    then
-        git clone https://$3@github.com/$1/$2.git
-    else
-        git clone https://github.com/$1/$2.git    
-    fi
+    #if [ "x$3" != "x" ]
+    #then
+    #    git clone https://$3@github.com/$1/$2.git
+    #else
+    #    git clone https://github.com/$1/$2.git    
+    #fi
+    git clone git@github.com:$1/$2.git     # use SSH key
     tar czf $2-$DATE.tar.gz $2
     rm -rf $2
 }
 
 function f_bitbucket()
 {
-    git clone https://$1@bitbucket.com/$1/$2.git
+    #git clone https://$1@bitbucket.com/$1/$2.git
+    git clone git@bitbucket.com:$1/$2.git  # use SSH key
     tar czf $2-$DATE.tar.gz $2
     rm -rf $2
 }
@@ -38,6 +40,7 @@ function f_clifton()
     rm -rf $2
 }
 
+# deja dans l'archive clifton....
 f_clifton boman MetaforSetup
 f_clifton boman keygen
 f_clifton boman mumps-4.10.0
@@ -45,21 +48,23 @@ f_clifton boman tetgen-1.4.3
 f_clifton boman triangle-1.6
 f_clifton boman parasolid
 
-f_github rboman math0471    rboman
-f_github rboman femcode     rboman
-f_github rboman ceci        rboman
-f_github rboman gmshio      rboman
-f_github rboman progs       rboman
-f_github rboman plot-applet rboman
+# github public
+f_github rboman math0471    #rboman
+f_github rboman femcode     #rboman
+f_github rboman ceci        #rboman
+f_github rboman gmshio      #rboman
+f_github rboman progs       #rboman
+f_github rboman plot-applet #rboman
 
-f_github math0471 waves rboman
+# github privé
+f_github ulgltas waves #rboman
+f_github ulgltas linuxbin #rboman
 
+# bitbucket (privé)
 f_bitbucket rboman lamtools
 f_bitbucket rboman math0024 
 f_bitbucket rboman idm 
-
-
-
+f_bitbucket rboman CT
 
 
 
