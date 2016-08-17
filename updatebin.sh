0cdab2bb2d1a110a9c13165e550cc2e31282005d
Romain Boman <romain.boman@gmail.com>
2016-08-17 09:57:32 +0200

    gestion git de linuxbin

--- updatebin.sh
+++ updatebin.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 echo "===== updating local bin..."
-svn update ~/bin
+#svn update ~/bin
+cd ~/bin ; git pull
 echo "===== updating blueberry..."
-ssh blueberry 'svn update ~/bin'
+#ssh blueberry 'svn update ~/bin'
+ssh blueberry 'cd ~/bin ; git pull'
 echo "===== updating thorgal..."
-ssh thorgal 'svn update ~/bin'
+#ssh thorgal 'svn update ~/bin'
+ssh thorgal 'cd ~/bin ; git pull'
 echo "===== updating fabulous..."
-ssh fabulous 'module load subversion ; svn update ~/bin'
+#ssh fabulous 'module load subversion ; svn update ~/bin'
+ssh fabulous 'cd ~/bin ; git pull'
 echo "===== updating spring..."
-ssh spring 'svn update ~/bin'
+#ssh spring 'svn update ~/bin'
+ssh spring 'cd ~/bin ; git pull'
 echo "===== done."
 
