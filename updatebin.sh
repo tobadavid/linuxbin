7e9b2972221e550cfd29d5fe108c84e0b1c19af9
Romain Boman <romain.boman@gmail.com>
2016-08-17 09:58:44 +0200

    correction fabulous

--- updatebin.sh
+++ updatebin.sh
@@ -10,7 +10,7 @@ echo "===== updating thorgal..."
 ssh thorgal 'cd ~/bin ; git pull'
 echo "===== updating fabulous..."
 #ssh fabulous 'module load subversion ; svn update ~/bin'
-ssh fabulous 'cd ~/bin ; git pull'
+ssh fabulous 'module load git ; cd ~/bin ; git pull'
 echo "===== updating spring..."
 #ssh spring 'svn update ~/bin'
 ssh spring 'cd ~/bin ; git pull'
