5dc75d876a7c3f790572755d9f3f5e27c1cad8af
Romain Boman <romain.boman@gmail.com>
2016-09-15 09:35:58 +0200

    ajout nouvelle machine gaston

--- updatebin.sh
+++ updatebin.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 echo "===== updating local bin..."
-#svn update ~/bin
 cd ~/bin ; git pull
 echo "===== updating blueberry..."
-#ssh blueberry 'svn update ~/bin'
 ssh blueberry 'cd ~/bin ; git pull'
 echo "===== updating thorgal..."
-#ssh thorgal 'svn update ~/bin'
 ssh thorgal 'cd ~/bin ; git pull'
 echo "===== updating fabulous..."
-#ssh fabulous 'module load subversion ; svn update ~/bin'
 ssh fabulous 'module load git ; cd ~/bin ; git pull'
 echo "===== updating spring..."
-#ssh spring 'svn update ~/bin'
 ssh spring 'cd ~/bin ; git pull'
+echo "===== done."
+ssh gaston 'cd ~/bin ; git pull'
 echo "===== done."
 
