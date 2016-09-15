a6bcaaa27eb7607c0fbea70938dac02b8ce754eb
Romain Boman <romain.boman@gmail.com>
2016-09-15 11:42:12 +0200

    update position repos

--- rb/bck_rep_git.sh
+++ rb/bck_rep_git.sh
@@ -48,23 +48,26 @@ f_blueberry boman tetgen-1.4.3
 f_blueberry boman triangle-1.6
 f_blueberry boman parasolid
 
-# github public
-f_github rboman math0471    #rboman
-f_github rboman femcode     #rboman
-f_github rboman ceci        #rboman
-f_github rboman gmshio      #rboman
-f_github rboman progs       #rboman
-f_github rboman plot-applet #rboman
+# github perso
+f_github rboman progs
+f_github rboman femcode
+f_github rboman fsi
+f_github rboman math0471
+f_github rboman plot-applet
+f_github rboman gmshio
 
-# github privé
-f_github ulgltas waves #rboman
-f_github ulgltas linuxbin #rboman
+# github a&m
+f_github ulgltas linuxbin
+f_github ulgltas waves
+f_github ulgltas plotter2d
+f_github ulgltas ceci     
+f_github ulgltas PFEM     
 
 # bitbucket (privé)
 f_bitbucket rboman lamtools
-f_bitbucket rboman math0024 
-f_bitbucket rboman idm 
 f_bitbucket rboman CT
+f_bitbucket rboman idm 
+f_bitbucket rboman math0024 
 
 
 
